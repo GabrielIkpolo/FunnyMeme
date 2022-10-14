dddbfb3b0689a7cbea8bf24a7e8f62b572919b39
GabrielIkpolo <contact.gikps@gmail.com>
2022-10-14 18:22:32 +0100

    implimented gif capture from camera

--- src/components/gif/Gif.jsx
+++ src/components/gif/Gif.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import './gif.css';
 import gifshot from 'gifshot';
+import downloadjs from 'downloadjs';
+import html2canvas from 'html2canvas';
+import {useCallback} from 'react';
 
 
 function Gif() {
 
     const handleVideoGIF = () => {
         gifshot.createGIF({
-            numFrames: 90,
+            numFrames: 70,
         }, (obj) => {
             if (!obj.error) {
                 var image = obj.image;
@@ -20,12 +23,20 @@ function Gif() {
         gifshot.stopVideoStreaming();
     }
 
+    const downloadGif = useCallback(async () => {
+        const theImage = document.querySelector('.theGIF > img');
+        if (!theImage) return alert("The image does not exist");
+        const canvas = await html2canvas(theImage);
+        const dataURL = canvas.toDataURL('image/png');
+        downloadjs(dataURL, 'download.png', 'image/png');
+    }, []);
+
     return (
         <>
             <div className="createGIF wrapper">
                 <button className='createGIFBtn col-6 offset-3' onClick={handleVideoGIF}>Create Video GIF</button>
                 <hr />
-                <div className='theGIF'>
+                <div className='theGIF' onClick={downloadGif}>
 
                 </div>
             </div>
