718581bfc7db2fcbf1c77495af84ae250ea52d91
GabrielIkpolo <contact.gikps@gmail.com>
2022-10-16 00:32:12 +0100

    Play on name

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,7 +4,7 @@ import  './footer.css';
 export default function Footer() {
     return (
         <div className='footer'>
-            <h5>Powerd by GIKPS</h5> 
+            <h5>Powerd by GIKPS and a disturbing Chukwuneyem Happiness</h5> 
             <p className="gnu"> &copy; ? Naaa! You are free to use and distribute as much as you want</p> 
         </div>
     )
